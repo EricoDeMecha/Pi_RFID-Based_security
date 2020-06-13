f4c95dd7b2c78a7fe5332d6b0ec919885b5030a7
EricoDeMecha <techcider4134@gmail.com>
2020-06-13 15:28:23 +0300

    WriteApp Display Error

--- pi_code/WriteApp/writeApp.py
+++ pi_code/WriteApp/writeApp.py
@@ -1,8 +1,35 @@
 from kivy.app import App
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 
-class Writer(AnchorLayout):
-    pass
+Builder.load_file('bios.kv')
+Builder.load_file('status.kv')
+
+class Bios(GridLayout):
+    reg = ObjectProperty(None)
+    serial_no = ObjectProperty(None)
+    phone_no = ObjectProperty(None)
+    cardData = ObjectProperty(None)
+    keyData = ObjectProperty(None)
+
+class Status(BoxLayout):
+    cardBtn = ObjectProperty(None)
+    keyBtn = ObjectProperty(None)
+class Writer(BoxLayout):
+    data_list = []
+    def __init__(self,**kwargs):
+        super(Writer,self).__init__(**kwargs)
+
+    def collectData(self):
+        self.data_list.extend([self.ids.bios.reg.text, self.ids.bios.serial_no.text, self.ids.bios.phone_no.text])
+        print(self.data_list)
+        print(self.ids.bios.cardData.text)
+        print(self.ids.bios.keyData.text)
+        #set button text
+        self.ids.status.cardBtn.text = 'CardWritten'
+        self.ids.status.keyBtn.text = 'KeyWritten'
 
 class writeApp(App):
     def build(self):
