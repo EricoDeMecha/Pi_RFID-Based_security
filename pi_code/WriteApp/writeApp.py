c84233d002352b29dfa44aa28561e85faf39cfe6
EricoDeMecha <techcider4134@gmail.com>
2020-06-16 15:57:54 +0300

    [Write_display_complete]

--- pi_code/WriteApp/writeApp.py
+++ pi_code/WriteApp/writeApp.py
@@ -1,8 +1,12 @@
+import threading
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 Builder.load_file('bios.kv')
 Builder.load_file('status.kv')
@@ -15,22 +19,51 @@ class Bios(GridLayout):
     keyData = ObjectProperty(None)
 
 class Status(BoxLayout):
-    cardBtn = ObjectProperty(None)
-    keyBtn = ObjectProperty(None)
+    Card = ObjectProperty(None)
+    Key  = ObjectProperty(None)
+
 class Writer(BoxLayout):
-    data_list = []
     def __init__(self,**kwargs):
         super(Writer,self).__init__(**kwargs)
-
     def collectData(self):
-        self.data_list.extend([self.ids.bios.reg.text, self.ids.bios.serial_no.text, self.ids.bios.phone_no.text])
-        print(self.data_list)
-        print(self.ids.bios.cardData.text)
-        print(self.ids.bios.keyData.text)
-        #set button text
-        self.ids.status.cardBtn.text = 'CardWritten'
-        self.ids.status.keyBtn.text = 'KeyWritten'
+        data_list = []
+        reg = self.ids.bios.reg.text
+        serial_no = self.ids.bios.serial_no.text
+        phone_no = self.ids.bios.phone_no.text
+        cardData = self.ids.bios.cardData.text
+        keyData = self.ids.bios.keyData.text
+        data_list.extend([reg, serial_no, phone_no, cardData, keyData])
+        if self.checkList(data_list):
+            popup = Popup(title = "Warning" , size_hint=(None, None), size=(160,160),
+                          content=Label(markup=True,text="[color=ff0000][b]Field(s) empty[/b][/color]"),
+                          auto_dismiss=True)
+            popup.open()
+        print(data_list)
+        #clear window content
+        self.clearWindowData()
+    def checkList(self, myList):
+        global flag
+        flag = False
+        for item in myList:
+            if not item:
+                flag = True
 
+        return flag
+    def clearWindowData(self):
+        self.ids.bios.reg.text = ''
+        self.ids.bios.serial_no.text = ''
+        self.ids.bios.phone_no.text = ''
+        self.ids.bios.cardData.text = ''
+        self.ids.bios.keyData.text = ''
+
+    def validate_item(self, item, lock):
+        #universal check
+        lock.acquire()
+        print(item)
+        lock.release()
+    def validate_reg(self):
+        lock = threading.Lock()
+        threading.Thread(target=self.validate_item, args=('reg',lock)).start()
 class writeApp(App):
     def build(self):
         return Writer()
